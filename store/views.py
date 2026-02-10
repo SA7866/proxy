80f89dd4f2c53ac7192d016b1bbed86607ed1708
Syed Ali <syedalile6@gmail.com>
2026-02-10 19:14:07 +0000

    Add products + shop page connected to admin

--- store/views.py
+++ store/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def shop(request):
+    products = Product.objects.all().order_by("-created_at")
+    return render(request, "store/shop.html", {"products": products})
