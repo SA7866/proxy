aa9f69452ca6631799f046c2d040bd136137da4b
Syed Ali <syedalile6@gmail.com>
2026-04-22 19:00:39 +0100

    Add EMAIL_TIMEOUT to prevent worker timeout on SMTP

--- config/settings.py
+++ config/settings.py
@@ -199,6 +199,7 @@ EMAIL_BACKEND   = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST      = 'smtp.gmail.com'
 EMAIL_PORT      = 587
 EMAIL_USE_TLS   = True
+EMAIL_TIMEOUT   = 10
 EMAIL_HOST_USER     = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL  = f'Proxy Store <{EMAIL_HOST_USER}>'
